47031d0582254f5dbc2db7bb4ed6d3bf36ef1a07
hoothuts <abimanyuananta02@gmail.com>
2024-12-18 07:27:12 +0700

    url('/Cashier') di sidebar
    hooh

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -3,7 +3,7 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
-        <a class="align-items-center d-flex m-0 navbar-brand text-wrap" href="{{ route('dashboard') }}">
+        <a class="align-items-center d-flex m-0 navbar-brand text-wrap" href="{{ url('/Cashier') }}">
             <img src="../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="...">
             <span class="ms-3 font-weight-bold">Soft UI Dashboard Laravel</span>
         </a>
@@ -12,7 +12,7 @@
     <div class="collapse navbar-collapse  w-auto" id="sidenav-collapse-main">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link {{ Request::is('Cashier') ? 'active' : '' }}" href="{{ url('/Cashier') }}">
+                <a class="nav-link {{ Request::is('Cashier') ? 'active' : '' }}" href="{{ url('/dashboard') }}">
                     <div
                         class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <svg width="12px" height="12px" viewBox="0 0 45 40" version="1.1"
